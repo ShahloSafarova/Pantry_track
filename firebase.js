933adb6ea1891dcfc3bb2354c180a43fbb168e52
Shahlo Safarova <152991623+ShahloSafarova@users.noreply.github.com>
2024-08-04 11:26:34 -0700

    auth was deleted
    deleted auth

--- firebase.js
+++ firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,8 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const firestore = getFirestore(app);
 const analytics = getAnalytics(app);
 
-export { firestore, auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut };
+export { firestore };
